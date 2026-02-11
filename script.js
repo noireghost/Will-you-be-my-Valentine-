1f195cd6450fa0383aa72383545f054f9f46157c
noireghost <andreeastelenco@gmail.com>
2026-02-12 00:42:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Sul serio?",
+    "Huhh????",
+    "Ayo nun scherzà",
+    "Dai bro..",
+    "Bruh",
+    "Salta",
+    "Buttati",
+    "Mauuu :(",
+    "Ok.",
+    "Le tue palle verranno fatte esplodere :)",
 ];
 
 let messageIndex = 0;
